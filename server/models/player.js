427b657d6ba90971dda9fe09c006a67eb848f495
Chris <gatto_c@yahoo.ca>
2016-01-01 18:32:26 -0400

    dev commit

--- server/models/player.js
+++ server/models/player.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 var bcrypt   = require('bcrypt');
+var Schema = mongoose.Schema;
 
 var PlayerSchema = new Schema({
   username: {type: String, required: true, trim: true},
